08efdde7d033096fce9514c5cd3a19b00a609b05
jurhnifer11 <jcforsyth@ucsd.edu>
2026-08-05 11:25:00 -0400

    Remove CSP navigate-to settings

--- src/components/email-display.tsx
+++ src/components/email-display.tsx
@@ -260,7 +260,7 @@ export default function EmailDisplay({
           className='h-72 flex-grow'
           onLoad={didLoad}
           sandbox='allow-same-origin'
-          {...{csp: "default-src 'none'; style-src 'unsafe-inline'; navigate-to 'none'; img-src https: data:;"}}
+          {...{csp: "default-src 'none'; style-src 'unsafe-inline'; img-src https: data:;"}}
         />
       );
     }
